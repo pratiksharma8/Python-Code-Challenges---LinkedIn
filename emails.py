c7dfe8b82b1371c4cfe5a88ebcbc0997a846ec6d
Pratik Sharma <55416418+pratiksharma8@users.noreply.github.com>
2020-07-21 14:28:39 -0700

    Update emails.py

--- emails.py
+++ emails.py
@@ -1,7 +1,7 @@
 import smtplib
 
-SENDER_EMAIl = 'pratiksharma8@gmail.com'
-SENDER_PASSWORD = 'Patrick@510'
+SENDER_EMAIl = 'YOUR EMAIL'
+SENDER_PASSWORD = 'YOUR PASSWORD'
 
 
 def send_email(receiver_email, subject, body):
@@ -11,4 +11,4 @@ def send_email(receiver_email, subject, body):
         server.sendmail(SENDER_EMAIl, receiver_email, message)
 
 
-send_email('ghimiresurakshya@gmail.com', 'AHAHAHAHAH', 'She is so cute.. AHAAHHAHAHAHA')
+send_email('RECEIVER EMAIL', 'SUBJECT', 'BODY')
